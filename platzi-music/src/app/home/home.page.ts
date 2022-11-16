b71f9e302b4f33d3ee1d85fcf0a024812cf53561
germanUNAP <ccus.german.chura@gmail.com>
2022-11-17 02:37:29 +1300

    slides with a javascript array created

--- platzi-music/src/app/home/home.page.ts
+++ platzi-music/src/app/home/home.page.ts
@@ -6,7 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  slideOps ={
+    initialSlide: 0,
+    slidePreview: 1,
+    centerdSlides: true,
+    speed: 400
+  };
+  slides = [
+    {
+      imageSrc: "assets/icon/keynote.jpg",
+      imageAlt: "Platzi music logo",
+      title: "Listen yout music",
+      subtitle: "ANYWHERE YOU WANT TO PLAY",
+      description: 'Best songs, best artists, also you can mix the songs you like however you want.',
+      icon: "play"
+    },
+    {
+      imageSrc: "assets/icon/keynote.jpg",
+      imageAlt: "Platzi music logo",
+      title: "Watch your videos",
+      subtitle: "CHOOSE YOUR VIDEOS TO WATCH",
+      description: 'See any videos you want, best content creator and soon',
+      icon: "videocam"
+    },
+    {
+      imageSrc: "assets/icon/keynote.jpg",
+      imageAlt: "Platzi music logo",
+      title: "SOON",
+      subtitle: "",
+      description: "",
+      icon: ""
+    }
+  ];
   constructor() {}
 
 }
