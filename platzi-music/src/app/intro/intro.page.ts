95d11b8095c4c4e85e9c0a49964b1485abeb8b83
germanUNAP <ccus.german.chura@gmail.com>
2022-11-22 01:24:43 +1300

    many warning messages thata cant be solved, so if someaone catchs whats the problem, please fix it

--- platzi-music/src/app/intro/intro.page.ts
+++ platzi-music/src/app/intro/intro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {Storage} from "@ionic/storage";
 
 @Component({
   selector: 'app-intro',
@@ -6,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./intro.page.scss'],
 })
 export class IntroPage implements OnInit {
-
-  constructor() { }
-
+  slideOps ={
+    initialSlide: 0,
+    slidePreview: 1,
+    centerdSlides: true,
+    speed: 400
+  };
+  slides = [
+    {
+      imageSrc: "assets/icon/keynote.jpg",
+      imageAlt: "Platzi music logo",
+      title: "Listen yout music",
+      subtitle: "ANYWHERE YOU WANT TO PLAY",
+      description: 'Best songs, best artists, also you can mix the songs you like however you want.',
+      icon: "play"
+    },
+    {
+      imageSrc: "assets/icon/keynote.jpg",
+      imageAlt: "Platzi music logo",
+      title: "Watch your videos",
+      subtitle: "CHOOSE YOUR VIDEOS TO WATCH",
+      description: 'See any videos you want, best content creator and soon',
+      icon: "videocam"
+    },
+    {
+      imageSrc: "assets/icon/keynote.jpg",
+      imageAlt: "Platzi music logo",
+      title: "SOON",
+      subtitle: "",
+      description: "",
+      icon: ""
+    }
+  ];
+  constructor(private router: Router, private storage: Storage) { }
+  finish(){
+    this.storage.set('isIntroShowed', true);
+    this.router.navigateByUrl("/home");
+  }
   ngOnInit() {
   }
 
